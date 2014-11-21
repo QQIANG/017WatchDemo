33c4c17db28eee641521854812db4f540e7b752e
JNYJ(William.zhou) <qqiang.dev@gmail.com>
2014-11-21 11:04:27 +0800

    Create watch demo

---
+++ WatchDemo/WatchDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  WatchDemo
+//
+//  Created by JNYJ on 14-11-19.
+//  Copyright (c) 2014年 JNYJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+
+		/*
+		{ categorys: [
+		{
+		category_id: "all",
+		category_name: "ALL",
+		category_desc: "All Categories"
+		},
+		{
+		category_id: "1",
+		category_name: "Art",
+		category_desc: "Artffsdfsdfds "
+		},
+		{
+		category_id: "4",
+		category_name: "Education",
+		category_desc: "Education for children and adults"
+		}]
+		}
+		*/
+
+
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
